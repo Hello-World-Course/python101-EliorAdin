fad683a50fe4b4ab518a226633e8c3016c9e8264
Elior Adin <elior.adin72@gmail.com>
2024-08-20 20:28:22 +0300

    hello world git 5

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -55,27 +55,3 @@ def register_user():
 
 user_name, user_board_size, user_num_mines = register_user()
 print(f"name:{user_name}, board size:{user_board_size}, number of mines:{user_num_mines}")
-"""name = input('Hello, whats your name')
-
-if len(name) <= 2:
-    name = None
-    board_size = None
-    number_of_mines = None
-    print("Your name is too short")
-else:
-    board_size = int(input(f"{name}, please choose board size"))
-    if board_size > 0 and board_size < 26:
-        number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
-        grid_size = board_size**2
-        half_grid_size = grid_size/2
-        if number_of_mines > 0 and number_of_mines <= half_grid_size:
-            number_of_mines = number_of_mines
-        else:
-            number_of_mines = None
-            print(f"{name}, you have entered illegal number of mines")
-    else:
-        board_size = None
-        number_of_mines = None
-        print(f"{name}, you have entered illegal board size")
-"""
-
