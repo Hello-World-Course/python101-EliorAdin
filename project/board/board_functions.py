4338ae4dc8a64baa7ad53191454e9386595f812d
Elior Adin <elior.adin72@gmail.com>
2024-08-23 11:49:42 +0300

    hello world git 5

--- project/board/board_functions.py
+++ project/board/board_functions.py
@@ -1,4 +1,20 @@
 def is_on_board(x, y, board):
     # This is wrong fix it:
     if len(board) > x > -1:
-        return True
+        if len(board[x]) > y > -1:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def safe_set_value(x, y, value, board):
+    if len(board) > x > -1:
+        if len(board[x]) > y > -1:
+            board[x][y] = value
+            return True
+        else:
+            return False
+    else:
+        return False
+
