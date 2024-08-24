bbaf00b4d0b196b37d4a89d9ce6e73c43b9bc0fa
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2024-08-24 13:20:31 +0000

    initial commit lesson assignment_lesson_16

---
+++ project/model/cell.py
@@ -0,0 +1,15 @@
+# This is a start of a Cell, this code is WRONG
+# Fix it and add all the required capabilities
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        # This is a mistake
+        self.is_clicked = None
+
+    def is_clicked(self):
+        return self.is_clicked
+
+    def __str__(self):
+        # mistake
+        return "blah blah I dont know what to do"
---
+++ project/model/mine.py
@@ -0,0 +1,5 @@
+from project.model.cell import Cell
+
+
+class Mine(Cell):
+# A Mine is a type of a Cell, implement all the required methods
