e90fa8aa727a5ce32cbceacd36c99ec25b19d28a
Elior Adin <elior.adin72@gmail.com>
2024-08-27 21:19:36 +0300

    hello world git 5

--- project/ui/terminal.py
+++ project/ui/terminal.py
@@ -1,11 +1,9 @@
 #from matplotlib.pyplot import get_current_fig_manager
 #from sympy.physics.units import current
 
-import user_interaction
+import project.ui.user_interaction as ui
 from project.model.board import Board
 import board_ui as b_ui
-
-
 
 class Terminal():
 
@@ -16,7 +14,7 @@ class Terminal():
         self.board_size = None
 
     def init_game(self):
-            self.name, self.board_size, self.number_of_mines = user_interaction.register_user()
+            self.name, self.board_size, self.number_of_mines = ui.register_user()
             if  self.name is None or self.number_of_mines is None or self.board_size is None:
                 return "Failed to init game"
             else:
