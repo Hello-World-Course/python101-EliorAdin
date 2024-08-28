592262ef38c2217224a95077181411d8b4c14c8b
Elior Adin <elior.adin72@gmail.com>
2024-08-28 19:29:48 +0300

    hello world git 5

--- project/ui/terminal.py
+++ project/ui/terminal.py
@@ -3,6 +3,7 @@
 import project.ui.user_interaction as ui
 from project.model.board import Board
 import board_ui as b_ui
+from project.model.mine import Mine
 
 class Terminal:
 
@@ -14,15 +15,18 @@ class Terminal:
 
     def init_game(self):
             self.name, self.board_size, self.number_of_mines = ui.register_user()
-            if  self.name is not None or self.number_of_mines is not None or self.board_size is not None:
+            if self.name is not None or self.number_of_mines is not None or self.board_size is not None:
                 self.current_board = Board(self.board_size)
             else:
                 return "Failed to init game"
 
 
     def create_string_(self):
-        return b_ui.draw_board(self.current_board)
+        if self.name is not None or self.number_of_mines is not None or self.board_size is not None:
+            return b_ui.draw_board(self.current_board)
 
     def draw(self):
         print(self.create_string_())
+
+
 
