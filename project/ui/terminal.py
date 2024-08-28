d23ba774769a4d1575bb32198c38c4a91e1bd9a0
Elior Adin <elior.adin72@gmail.com>
2024-08-28 19:11:40 +0300

    hello world git 5

--- project/ui/terminal.py
+++ project/ui/terminal.py
@@ -18,11 +18,10 @@ class Terminal():
 
     def init_game(self):
             self.name, self.board_size, self.number_of_mines = ui.register_user()
-            if  self.name is None or self.number_of_mines is None or self.board_size is None:
-                return "Failed to init game"
-            else:
+            if  self.name is not None or self.number_of_mines is not None or self.board_size is not None:
                 self.current_board = Board(self.board_size)
-
+            else:
+                return "Failed to init game"
 
 
     def create_string_(self):
